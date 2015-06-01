5f9af13e992669f4d27835bd5474d2efd7fd6b91
y@su <toyasyu@gmail.com>
2015-06-01 22:41:14 +0900

    aliases.zsh: Add 'aliases.zsh'

---
+++ oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,15 @@
+## alias
+case "${OSTYPE}" in
+freebsd*|darwin*)
+  alias ll="ls -alG"
+  alias ls="ls -G -w"
+  ;;
+linux*)
+  alias ll="ls -al --color"
+  alias ls="ls --color"
+  ;;
+esac
+alias h="history 0"
+alias be="bundle exec"
+alias bi="bundle install"
+alias r="bin/rails"
